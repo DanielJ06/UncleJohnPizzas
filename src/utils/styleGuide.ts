9fe0121d008cbff2ab0844031eeda748ae6be042
danieljesus <daniel.j.rodrigues06@gmail.com>
2021-05-26 11:35:53 -0300

    Add colors resources

---
+++ src/utils/styleGuide.ts
@@ -0,0 +1,9 @@
+export const Colors = {
+  red: "#F5313F",
+  orange: "#FFA360",
+  purple: "#6D6E9C",
+  lightPurple: "#A0A8CC",
+  stroke: "#DADAE5",
+  lightGrey: "#F4F3F9",
+  white: "#FFFFFF"
+}
